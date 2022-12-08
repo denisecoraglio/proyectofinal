0ec017d9fe6bdfb5a56bc88d9a52bdeb1dc35b1a
denisecoraglio <denisecoraglio@gmail.com>
2022-12-08 20:14:24 -0300

    actualizacion de base de datos

---
+++ project/seed_data.py
@@ -0,0 +1,6 @@
+from ejemplo.models import Familiar
+Familiar(nombre="German", direccion="Av Las Violetas N° 306", numero_pasaporte= 29446657).save()
+Familiar(nombre="Felipe", direccion="Av Las Violetas N° 306", numero_pasaporte= 54934733).save()
+Familiar(nombre="Edith", direccion="25 de MAyo N° 745", numero_pasaporte= 12859327).save()
+Familiar(nombre="Osvaldo", direccion="25 de MAyo N° 745", numero_pasaporte=12469880).save()
+print("Se cargo con éxito los usuarios de pruebas")
